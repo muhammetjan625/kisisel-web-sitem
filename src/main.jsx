a718d14352c797758e562e4d85c3f6eefca4f804
sokratests0-arch <sokrates.ts0@gmail.com>
2026-01-26 17:04:32 +0300

    Router fix: HashRouter'a gecis yapildi

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './context/AuthContext'; // 1. BU SATIRI IMPORT ET
 
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <AuthProvider> {/* 2. BURAYA EKLE (App'i sarmalamalı) */}
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider> {/* 3. BURADA KAPAT */}
     </HelmetProvider>
   </React.StrictMode>,
